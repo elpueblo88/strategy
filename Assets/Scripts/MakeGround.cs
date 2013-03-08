27c0b2bd825f1ce7869f68cd783b5c9c03973094
Daniel Pfeffer <dnp19@case.edu>
2013-03-08 00:57:17 -0500

    Grid with basic textures

--- Assets/Scripts/MakeGround.cs
+++ Assets/Scripts/MakeGround.cs
@@ -5,11 +5,14 @@ public class MakeGround : MonoBehaviour {
 	
 	public int xLength;
 	public int zLength;
+	public Vector3 BLcorner;
 	public GameObject terrain;
 	
 	GameObject[,] grid;
 	float xSize;
 	float zSize;
+	
+	Vector2[] GoalSpots = {new Vector2(7,7), new Vector2(3,3), new Vector2(3,10), new Vector2(11,3), new Vector2(11,11)};
 
 	// Use this for initialization
 	void Start () 
@@ -31,19 +34,8 @@ public class MakeGround : MonoBehaviour {
 		zSize = terrain.transform.localScale.z;
 		grid = new GameObject[zLength, xLength];
 		
-		int i, j;
-		Vector3 spot = Vector3.zero;
-		
-		for(i = zLength - 1; i >= 0; i--)
-		{
-			for(j = 0; j < xLength; j++)
-			{
-				grid[i,j] = Instantiate(terrain) as GameObject;
-				spot.x = (j * xSize);
-				spot.z = (i * zSize);
-				grid[i,j].transform.localPosition = spot;
-			}
-		}
+		makeGrid ();
+		modifyGrid();
 		
 		
 	}
@@ -52,5 +44,32 @@ public class MakeGround : MonoBehaviour {
 	void Update () 
 	{
 		
+	}
+	
+	void makeGrid()
+	{
+		int i, j;
+		Vector3 spot = Vector3.zero;
+		
+		for(i = zLength - 1; i >= 0; i--)
+		{
+			for(j = 0; j < xLength; j++)
+			{
+				grid[i,j] = Instantiate(terrain) as GameObject;
+				spot.x = (j * xSize + BLcorner.x);
+				spot.z = (i * zSize + BLcorner.z);
+				spot.y = BLcorner.y;
+				grid[i,j].transform.localPosition = spot;
+			}
+		}
+	}
+	
+	void modifyGrid()
+	{
+		int i;
+		for(i = 0; i < 5; i++)
+		{
+			grid[(int)GoalSpots[i].y, (int)GoalSpots[i].x].renderer.material = Resources.Load("radioactiveSpot", typeof(Material)) as Material;	
+		}
 	}
 }
