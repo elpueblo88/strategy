c180fa3a95fea4a813619e7efdd7aa8945b57974
Rachid Lamouri <rachidlamouri@gmail.com>
2013-04-04 04:54:19 -0400

    Updated interface, added a help menu

--- Assets/Scripts/Interface.cs
+++ Assets/Scripts/Interface.cs
@@ -6,14 +6,44 @@ public class Interface : MonoBehaviour {
 	private const float BOX_Y = .60f;
 	private const float BOX_WIDTH = .20f;
 	private const float BOX_HEIGHT = .40f;
-	
-	private const float LABEL_HEIGHT = .04f;
+
 	private const float BUTTON_HEIGHT = .10f;
 	
-	private const float HELP_BOX_X = .95f;
-	private const float HELP_BOX_Y = .05f;
-	private const float HELP_BOX_WIDTH = .05f;
-	private const float HELP_BOX_HEIGHT = .05f;
+	private const float HELP_BOX1_X = .95f;
+	private const float HELP_BOX1_Y = 0;
+	private const float HELP_BOX1_WIDTH = .05f;
+	private const float HELP_BOX1_HEIGHT = .05f;
+	
+	private const float HELP_BOX2_X = BOX_X;
+	private const float HELP_BOX2_Y = 0;
+	private const float HELP_BOX2_WIDTH = BOX_WIDTH;
+	private const float HELP_BOX2_HEIGHT = BOX_Y;
+	
+	private string[] HELP_LABELS ={
+		"Controls:",
+		"w,a,s,d: pan camera",
+		"click mouse (context sensitive):",
+		"\tselect unit",
+		"\tmove unit",
+		"\tselect unit to attack",
+		"\tselect terrain (view info)",
+		"h: close help",
+		"",
+		"Objective:",
+		"Use your radioactive child to capture 3",
+		"\t radioactive spots.",
+		"OR:Kill all enemy units.",
+		"Tie: both radio-child units die."
+	};
+	
+	private const int INFO_TYPE_NONE = 0;
+	private const int INFO_TYPE_UNIT = 1;
+	private const int INFO_TYPE_TERRAIN = 2;
+	
+	private const KeyCode KEY_HELP = KeyCode.H;
+	
+	private bool displayHelp = false;
+	private int infoType;
 	
 	//private float[] unitInfo;
 	
@@ -23,12 +53,36 @@ public class Interface : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown(KEY_HELP)){
+			displayHelp = !displayHelp;
+		}
 	}
 	
 	void OnGUI(){
+		if(displayHelp){
+			GUI.Box(new Rect(Screen.width*HELP_BOX2_X,Screen.height*HELP_BOX2_Y,Screen.width*HELP_BOX2_WIDTH,Screen.height*HELP_BOX2_HEIGHT),"");
+			float labelHeight = HELP_BOX2_HEIGHT/HELP_LABELS.Length;
+			for(int i = 0; i < HELP_LABELS.Length; i++){
+				GUI.Label(new Rect(Screen.width*HELP_BOX2_X,Screen.height*HELP_BOX2_Y + i*Screen.height*labelHeight,Screen.width*HELP_BOX2_WIDTH,Screen.height*labelHeight),HELP_LABELS[i]);
+			}
+		}else{
+			GUI.Box(new Rect(Screen.width*HELP_BOX1_X,Screen.height*HELP_BOX1_Y,Screen.width*HELP_BOX1_WIDTH,Screen.height*HELP_BOX1_HEIGHT),"h: help");
+		}
 		
 		GUI.Box(new Rect(Screen.width*BOX_X,Screen.height*BOX_Y,Screen.width*BOX_WIDTH,Screen.height*BOX_HEIGHT), "");
+		
+		if(GUI.Button(new Rect(Screen.width*BOX_X,Screen.height*BOX_Y + Screen.height*(BOX_HEIGHT - BUTTON_HEIGHT),Screen.width*BOX_WIDTH,Screen.height*BUTTON_HEIGHT),"End Turn")){
+			this.gameObject.SendMessage("changeFromPlayerOne");
+		}
+		
+		switch(infoType){
+		case INFO_TYPE_NONE:
+			break;
+		case INFO_TYPE_UNIT:
+			break;
+		case INFO_TYPE_TERRAIN:
+			break;
+		}
 		/*
 		unitInfo = new float[]{1,10,2,3,1};
 		
@@ -70,12 +124,18 @@ public class Interface : MonoBehaviour {
 		GUI.Label(new Rect(Screen.width*BOX_X,Screen.height*BOX_Y + 4*Screen.height*LABEL_HEIGHT,Screen.width*BOX_WIDTH,Screen.height*LABEL_HEIGHT),unitMovement);
 		*/
 		
-		if(GUI.Button(new Rect(Screen.width*BOX_X,Screen.height*BOX_Y + Screen.height*(BOX_HEIGHT - BUTTON_HEIGHT),Screen.width*BOX_WIDTH,Screen.height*BUTTON_HEIGHT),"End Turn")){
-			this.gameObject.SendMessage("changeFromPlayerOne");
-		}
+		
 	}
 	
 	private void updateUnitInfo(float[] unitInfo){
 		//this.unitInfo = unitInfo;
+	}
+	
+	private void updateTerrainInfo(){
+		
+	}
+	
+	private void cancelInfo(){
+		
 	}
 }
