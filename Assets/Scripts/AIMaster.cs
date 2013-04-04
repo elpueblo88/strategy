f673593581b1081d5210646e26bdeb0f18a22784
Rachid Lamouri <rachidlamouri@gmail.com>
2013-04-04 02:41:15 -0400

    AI movement should be working

--- Assets/Scripts/AIMaster.cs
+++ Assets/Scripts/AIMaster.cs
@@ -33,57 +33,69 @@ public class AIMaster : MonoBehaviour {
 				
 				LinkedList<Vector2> positionList = gameMasterScript.changeGroundColorMaster(new PlayerMovement((int)unitScript.location.x,(int)unitScript.location.y, unitScript.moveSpeed, 2));
 				
-				Vector2 nextPosition = new Vector2();
 				
-				locationToCheckAgainst = unitScript.location;
-				if(unitScript.unitType == "radioChild"){
-					if(positionList.Count != 0){
+				/* Determines a target position (for moving to if it is a radiochild, or attacking otherwise)
+				 * For every unit type besides the rogue, the if statement is just a way to iterate through the
+				 * 		different priority targets: 
+				 * 		Example: for the rogue, it looks for a bomber to attack, and if it can't find one it looks for a brain and so on
+				 * 		
+				 */
+			
+				if(positionList.Count != 0){
+					Vector2 targetPosition = new Vector2(-1,-1);
+					locationToCheckAgainst = unitScript.location;
+					if(unitScript.unitType == "radioChild"){
 						foreach(Vector2 goalLocation in makeGroundScript.goalLocations){
 							if(makeGroundScript.grid[(int)goalLocation.y,(int)goalLocation.x].GetComponent<TerrainScript>().taken == 0){
-								if(nextPosition == null || (Vector2.Distance(nextPosition,unitScript.location) > Vector2.Distance(unitScript.location,goalLocation))){
-									nextPosition = goalLocation;
+								if(targetPosition.x == -1 || (Vector2.Distance(targetPosition,unitScript.location) > Vector2.Distance(unitScript.location,goalLocation))){
+									targetPosition = goalLocation;
 								}
 							}
+						}					
+					}else if(unitScript.unitType == "rogue"){
+						if((targetPosition = findTarget("bomber")).x != -1 
+							|| (targetPosition = findTarget("brain")).x != -1 
+							|| (targetPosition = findTarget("radioChild")).x != -1  
+							|| (targetPosition = findTarget("rogue")).x != -1
+							|| (targetPosition = findTarget("brute")).x != -1){
 						}
-					}else{
+					}else if(unitScript.unitType == "bomber"){
+						if((	targetPosition = findTarget("radioChild")).x != -1 
+							|| (targetPosition = findTarget("brute")).x != -1 
+							|| (targetPosition = findTarget("rogue")).x != -1  
+							|| (targetPosition = findTarget("brain")).x != -1
+							|| (targetPosition = findTarget("bomber")).x != -1){
+						}
+					}else if(unitScript.unitType == "brain"){
+						if((	targetPosition = findTarget("brute")).x != -1 
+							|| (targetPosition = findTarget("radioChild")).x != -1 
+							|| (targetPosition = findTarget("brain")).x != -1  
+							|| (targetPosition = findTarget("bomber")).x != -1
+							|| (targetPosition = findTarget("rogue")).x != -1){
+						}
+					}else if(unitScript.unitType == "brute"){
+						if((	targetPosition = findTarget("radioChilld")).x != -1 
+							|| (targetPosition = findTarget("rogue")).x != -1 
+							|| (targetPosition = findTarget("brute")).x != -1  
+							|| (targetPosition = findTarget("bomber")).x != -1
+							|| (targetPosition = findTarget("brain")).x != -1){
+						}
+					}
+					
+					if(targetPosition.x != -1){
+						Vector2 nextPosition = new Vector2(-1,-1);
+						foreach(Vector2 location in positionList){
+							if(nextPosition.x == -1 || (Vector2.Distance(nextPosition,targetPosition) > Vector2.Distance(location,targetPosition))){
+								nextPosition = location;
+							}
+						}
 						
-					}
-					
-					
-				}else if(unitScript.unitType == "rogue"){
-					
-					if((	nextPosition = findTarget("bomber")) != null 
-						|| (nextPosition = findTarget("brain")) != null 
-						|| (nextPosition = findTarget("radioChild")) != null  
-						|| (nextPosition = findTarget("rogue")) != null
-						|| (nextPosition = findTarget("brute")) != null){
-					}
-				}else if(unitScript.unitType == "bomber"){
-					if((	nextPosition = findTarget("radioChild")) != null 
-						|| (nextPosition = findTarget("brute")) != null 
-						|| (nextPosition = findTarget("rogue")) != null  
-						|| (nextPosition = findTarget("brain")) != null
-						|| (nextPosition = findTarget("bomber")) != null){
-					}
-				}else if(unitScript.unitType == "brain"){
-					if((	nextPosition = findTarget("brute")) != null 
-						|| (nextPosition = findTarget("radioChild")) != null 
-						|| (nextPosition = findTarget("brain")) != null  
-						|| (nextPosition = findTarget("bomber")) != null
-						|| (nextPosition = findTarget("rogue")) != null){
-					}
-				}else if(unitScript.unitType == "brute"){
-					if((	nextPosition = findTarget("radioChilld")) != null 
-						|| (nextPosition = findTarget("rogue")) != null 
-						|| (nextPosition = findTarget("brute")) != null  
-						|| (nextPosition = findTarget("bomber")) != null
-						|| (nextPosition = findTarget("brain")) != null){
+						if(nextPosition.x != -1){
+							unit.SendMessage("moveTo",nextPosition);
+						}
 					}
 				}
 				
-				if(nextPosition != null){
-					unit.SendMessage("moveTo",nextPosition);
-				}
 				
 			
 				// attack if possible
@@ -95,12 +107,12 @@ public class AIMaster : MonoBehaviour {
 	}
 	
 	private Vector2 findTarget(string targetType){
-		Vector2 nextTarget = new Vector2();
+		Vector2 nextTarget = new Vector2(-1,-1);
 		
 		this.team1 = gameMaster.GetComponent<UnitMaster>().team1;	
 		foreach(GameObject unit in team1){
 			UnitScript unitScript = unit.GetComponent<UnitScript>();
-			if(unitScript.unitType == targetType && (nextTarget == null || Vector2.Distance(nextTarget,locationToCheckAgainst) > Vector2.Distance(locationToCheckAgainst,unitScript.location))){
+			if(unitScript.unitType == targetType && (nextTarget.x == -1 || Vector2.Distance(nextTarget,locationToCheckAgainst) > Vector2.Distance(locationToCheckAgainst,unitScript.location))){
 				nextTarget = unitScript.location;
 			}
 		}
