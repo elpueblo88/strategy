52b7e610e47f7cf4902a1b0e4fb89ef640f1ede6
Rachid Lamouri <rachidlamouri@gmail.com>
2013-04-04 00:58:36 -0400

    More AI scripting

--- Assets/Scripts/AIMaster.cs
+++ Assets/Scripts/AIMaster.cs
@@ -5,9 +5,12 @@ using System.Collections.Generic;
 public class AIMaster : MonoBehaviour {
 	private const int PRIORITY_ROGUE = 1;
 	
+	private GameObject[] team1;
 	private GameObject[] team2;
 	private bool waitForUpdate;
 	private GameObject gameMaster;
+	
+	private Vector2 locationToCheckAgainst;
 	// Use this for initialization
 	void Start () {
 		gameMaster = GameObject.Find("GameMaster");
@@ -31,6 +34,8 @@ public class AIMaster : MonoBehaviour {
 				LinkedList<Vector2> positionList = gameMasterScript.changeGroundColorMaster(new PlayerMovement((int)unitScript.location.x,(int)unitScript.location.y, unitScript.moveSpeed, 2));
 				
 				Vector2 nextPosition = new Vector2();
+				
+				locationToCheckAgainst = unitScript.location;
 				if(unitScript.unitType == "radioChild"){
 					if(positionList.Count != 0){
 						foreach(Vector2 goalLocation in makeGroundScript.goalLocations){
@@ -46,6 +51,7 @@ public class AIMaster : MonoBehaviour {
 					
 					
 				}else if(unitScript.unitType == "rogue"){
+					
 					if((	nextPosition = findTarget("bomber")) != null 
 						|| (nextPosition = findTarget("brain")) != null 
 						|| (nextPosition = findTarget("radioChild")) != null  
@@ -91,6 +97,13 @@ public class AIMaster : MonoBehaviour {
 	private Vector2 findTarget(string targetType){
 		Vector2 nextTarget = new Vector2();
 		
+		this.team1 = gameMaster.GetComponent<UnitMaster>().team1;	
+		foreach(GameObject unit in team1){
+			UnitScript unitScript = unit.GetComponent<UnitScript>();
+			if(unitScript.unitType == targetType && (nextTarget == null || Vector2.Distance(nextTarget,locationToCheckAgainst) > Vector2.Distance(locationToCheckAgainst,unitScript.location))){
+				nextTarget = unitScript.location;
+			}
+		}
 		return nextTarget;
 	}
 }
